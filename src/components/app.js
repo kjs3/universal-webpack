2181af18cd1ffacb85f69eda20bc553fd48bfc81
Kenny Smith <mail@kjs3.com>
2017-03-09 10:38:35 -0500

    starting point for universal webpack setup

---
+++ src/components/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => {
+  return (
+    <main>
+      <h1>Universal webpack!!!</h1>
+    </main>
+  )
+}
---
+++ src/server.js
@@ -0,0 +1,43 @@
+import path from 'path'
+import debug from 'debug'
+import express from 'express'
+
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import App from './components/app'
+
+const log = debug('server')
+
+const server = express()
+server.set('views', path.resolve(__dirname, 'html'))
+server.set('view engine', 'pug')
+
+server.get('/', (req, res) => {
+  const html = renderToString(<App />)
+
+  res.render('index', {
+    pageTitle: 'Universal Webpack',
+    pageHtml: html
+  })
+})
+
+// ERROR HANDLERS
+
+// general server errors
+server.use((err, req, res, next) => {
+  log(`SERVER ERROR: ${err.stack}`)
+
+  res.status(500)
+  res.json({
+    error: 'server error'
+  })
+})
+
+server.set('port', process.env.PORT || 3000)
+
+server.listen(server.get('port'), function() {
+  const env = server.get('env')
+  const port = this.address().port
+
+  log(`Universal webpack server is running in ${env} on port ${port}`)
+})
